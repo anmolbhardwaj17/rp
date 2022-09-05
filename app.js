1e2a01fa8bb73b6657f4fa1d4e44150c7b4f2009
Anmol Bhardwaj <anmolbhardwaj17@gmail.com>
2022-09-06 00:20:58 +0530

    added db config

---
+++ app.js
@@ -0,0 +1,19 @@
+const dotenv = require('dotenv');
+const express = require('express');
+var cors = require('cors')
+const app = express();
+
+dotenv.config({path: './config.env'});
+
+require('./db/connection');
+app.use(express.json());
+const PORT = process.env.PORT;
+app.use(cors())
+
+app.get("/", async (req, res) =>{
+    res.send({message:"recruitment portal homepage", code:200});
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port http://localhost:${PORT}`) 
+})
